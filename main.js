2aa0b6920023afe13c9a5cafdda7302d5ad06b49
vansh4 <80308057+vansh4@users.noreply.github.com>
2022-01-08 17:28:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+ 
+}
+
+function setup(){
+canvas=createCanvas(640,480);
+canvas.position(110,250);
+}
+function draw(){
+fill(255,0,0);
+stroke(255,0,0);
+    circle(40, 40, 40);
+
+    }
